cafbd2572dc39682af1ee5074c669692b01f2bfb
dekokun <dekokun@hatena.ne.jp>
2022-01-17 22:47:15 +0900

    basic plugin implementation

--- mackerel-plugin-bitcoin-core.go
+++ mackerel-plugin-bitcoin-core.go
@@ -7,7 +7,106 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/btcsuite/btcd/rpcclient"
+	mp "github.com/mackerelio/go-mackerel-plugin"
 )
+
+type BitcoinCorePlugin struct {
+	Prefix   string
+	Dest     string
+	User     string
+	Password string
+}
+
+func (b BitcoinCorePlugin) GraphDefinition() map[string]mp.Graphs {
+	labelPrefix := strings.Title(b.MetricKeyPrefix())
+	return map[string]mp.Graphs{
+		"block": {
+			Label: labelPrefix + " block height",
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "height", Label: "height", Diff: false, Stacked: false},
+			},
+		},
+		"network": {
+			Label: labelPrefix + " network score",
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "score.*", Label: "score.*", Diff: false, Stacked: false},
+			},
+		},
+		"peer": {
+			Label: labelPrefix + " peer connections",
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "in", Label: "in", Diff: false, Stacked: true},
+				{Name: "out", Label: "out", Diff: false, Stacked: true},
+				{Name: "total", Label: "total", Diff: false, Stacked: false},
+			},
+		},
+	}
+}
+
+func (b BitcoinCorePlugin) FetchMetrics() (map[string]float64, error) {
+	connCfg := &rpcclient.ConnConfig{
+		Host:                b.Dest,
+		User:                b.User,
+		Pass:                b.Password,
+		DisableConnectOnNew: true,
+		HTTPPostMode:        true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:          true, // Bitcoin core does not provide TLS by default
+	}
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Shutdown()
+	stat := map[string]float64{}
+	// Get the current block count.
+	blockCount, err := client.GetBlockCount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stat["height"] = float64(blockCount)
+	info, err := client.GetNetworkInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%#v", info.LocalAddresses[0].Score)
+	for _, v := range info.LocalAddresses {
+		stat["network.score."+v.Address] = float64(v.Score)
+	}
+	peerInfo, err := client.GetPeerInfo()
+	total := 0
+	in := 0
+	out := 0
+	for _, v := range peerInfo {
+		if v.Inbound {
+			in += 1
+		} else {
+			out += 1
+		}
+		total += 1
+	}
+	stat["in"] = float64(in)
+	stat["out"] = float64(out)
+	stat["total"] = float64(total)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return stat, nil
+}
+
+func (b BitcoinCorePlugin) MetricKeyPrefix() string {
+	if b.Prefix == "" {
+		b.Prefix = "bitcoin"
+	}
+	return b.Prefix
+}
 
 var version string
 var commit string
@@ -34,12 +133,25 @@ func Run(ctx context.Context, argv []string, outStream, errStream io.Writer) err
 		fmt.Sprintf("%s (v%s rev:%s)", cmdName, version, commit), flag.ContinueOnError)
 	fs.SetOutput(errStream)
 	ver := fs.Bool("version", false, "display version")
+	optHost := fs.String("host", "localhost", "host to connect")
+	optPort := fs.String("port", "8332", "port to connect")
+	optUser := fs.String("user", "", "rpc user")
+	optPassword := fs.String("password", "", "rpc password")
+	optPrefix := flag.String("metric-key-prefix", "bitcoin", "Metric key prefix")
 	if err := fs.Parse(argv); err != nil {
 		return err
 	}
 	if *ver {
 		return printVersion(outStream)
 	}
+	b := BitcoinCorePlugin{
+		Prefix:   *optPrefix,
+		Dest:     *optHost + ":" + *optPort,
+		User:     *optUser,
+		Password: *optPassword,
+	}
+	plugin := mp.NewMackerelPlugin(b)
+	plugin.Run()
 	return nil
 }
 
